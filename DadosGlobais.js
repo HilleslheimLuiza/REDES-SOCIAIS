cf50736e8c54b3ce0bfd1532658e52620844905c
Hilleslheim <108075498+HilleslheimLuiza@users.noreply.github.com>
2024-10-15 16:53:33 -0300

    15.10

--- DadosGlobais.js
+++ DadosGlobais.js
@@ -5,7 +5,19 @@ console.log(url);
 async function visualizarInformacoesGlobais(){
     const res = await fetch(url);
     const dados = await res.json();
-    console.log(dados);
+    const paragrafo = document.createElement('p');
+    paragrafo.classList.add("graficos-container_texto");
+    paragrafo.innerHTML = `Você sabia que o mundo tem 
+    ${dados.total_pessoas_mundo} de pessoas e que aproximadamente
+    ${dados.total_pessoas_conectadas} estão conectados em 
+    alguma rede social e passam em média ${dados.tempo_medio}
+    horas conectadas` 
+
+    const container = document.getElementById('graficos-container');
+    container.appendChild(paragrafo);
+
+
+    console.log(paragrafo);
 } 
 
 visualizarInformacoesGlobais()
